f870d86fb071fbd37ae2ba129ac1335a932668f3
hayyaoe <hayya947@gmail.com>
2024-04-27 02:12:42 +0700

    Animating Section 2 Finish

--- Landmarks/Model/Hike.swift
+++ Landmarks/Model/Hike.swift
@@ -12,12 +12,12 @@ struct Hike: Codable, Hashable, Identifiable {
     var id: Int
     var name: String
     var distance: Double
-    var difficutly: Int
+    var difficulty: Int
     var observations: [Observation]
-    
+
     static var formatter = LengthFormatter()
-    
-    var distanceText: String{
+
+    var distanceText: String {
         Hike.formatter
             .string(fromValue: distance, unit: .kilometer)
     }
@@ -25,7 +25,7 @@ struct Hike: Codable, Hashable, Identifiable {
     // add Observation stuct with the parameters
     struct Observation: Codable, Hashable {
         var distanceFromStart: Double
-        
+
         var elevation: Range<Double>
         var pace: Range<Double>
         var heartRate: Range<Double>
---
+++ Landmarks/Views/HikeView.swift
@@ -0,0 +1,18 @@
+//
+//  HikeView.swift
+//  Landmarks
+//
+//  Created by RabiRabi Channel on 27/04/24.
+//
+
+import SwiftUI
+
+struct HikeView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    HikeView()
+}
